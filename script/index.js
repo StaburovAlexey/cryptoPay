af0e09ce0cea9d610009f904329107977b9713d9
Alexey Staburov <gilbertfrost@yandex.ru>
2023-10-11 16:35:47 +0300

    темная тема готова

--- script/index.js
+++ script/index.js
@@ -1,6 +1,10 @@
 const slider = new Swiper(".usecases__slider", {
   // Optional parameters
   direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
   slidesPerView: 1,
   // Slide class
   slideClass: "usecases__slider-slide",
@@ -24,20 +28,20 @@ const slider = new Swiper(".usecases__slider", {
       fractionDrow();
     },
   },
-  // breakpoints: {
-  //   320: { centeredSlides: true },
-  //   426: {},
-  //   768: { centeredSlides: false, spaceBetween: 10 },
-  //   1024: {},
-  // },
 });
 const additionalBtns = document.querySelectorAll(".additional__btn");
-
 const iconsContainer = document.querySelector(".features__curencies");
 const featuresIcons = iconsContainer.querySelectorAll(
   ".features__curencies-icon-conteiner"
 );
+const page = document.querySelector(".page");
+const btnTheme = document.querySelector(".theme-btn");
 
+btnTheme.addEventListener("click", (e) => {
+  page.classList.toggle("dark-theme");
+  btnTheme.classList.toggle("theme-btn_active")
+
+});
 
 function fractionDrow() {
   let activeSlide = document.querySelector(
@@ -55,18 +59,3 @@ function fractionDrow() {
     fractionUp.textContent = "03";
   }
 }
-// document.addEventListener("mouseover", (event) => {
-//   const target = event.target;
-//   if (target.classList.contains("features__curencies")) {
-//     for (let i = 0; i < featuresIcons.length; ++i) {
-//       const img = featuresIcons[i].querySelector(".features__curencies-icon");
-//       const text = featuresIcons[i].querySelector(
-//         ".features__curencies-icon-text"
-//       );
-//       setTimeout(() => {
-//         img.classList.add("features-icon");
-//         text.classList.add("features-icon-text");
-//       }, i * 400);
-//     }
-//   }
-// });
